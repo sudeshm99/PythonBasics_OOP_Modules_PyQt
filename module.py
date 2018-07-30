0975484d584b65a45608d5ce66896f9f73671d62
sudeshm99 <sudesh.madushanka25@gmail.com>
2018-07-30 22:41:46 +0530

    text read and write

--- module.py
+++ module.py
@@ -2,14 +2,24 @@
 
 print('file name: '+file.name)
 print('file closed: ',file.closed)
-print('file mode: '+file.mode)'''
+print('file mode: '+file.mode)
 
 
 file2 = open('1.txt','r+')
 
-word = file2.read(10)
+word = file2.read(50)
 print('--------------------')
 print(word)
 print('--------------------')
 position = file2.tell()
-print(position)
+print(position)'''
+
+
+file = open('1.txt','a')
+
+file.newlines
+file.softspace
+
+file.write('I am studing');
+
+file.close()
