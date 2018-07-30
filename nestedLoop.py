e8a7cd78a1b4167f826a1726bae71030a0fc42d5
sudeshm99 <sudesh.madushanka25@gmail.com>
2018-07-30 12:26:49 +0530

    loop

---
+++ nestedLoop.py
@@ -0,0 +1,35 @@
+'''x=0;
+y=0;
+while x<5:
+    print("a");
+    while y<10:
+        print("b");
+        y+=1;
+    y=0;        
+    x+=1;
+
+for letter in 'python':
+    print(letter);5 '''
+a=0;
+b=0;
+''' while a<4:
+    b=0;
+    while b<4:
+        print(a,b);
+        b+=1
+    a+=1
+
+
+for a in range(0,4):
+    b=0
+    for b in range(0,4):
+        print(a,b);
+        b+=1;
+    a+=1; '''
+
+'''for i in range(0,20):
+    for x in range(2,i):
+        if i%x==0:
+            print '''
+
+print("yes") if a==0 else print("")
